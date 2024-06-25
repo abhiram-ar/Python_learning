0be850e670fbd7198dd646045641c9b70be48c32
abhiram-ar <82314439+abhiram-ar@users.noreply.github.com>
2024-06-25 16:42:40 +0530

    exception

--- Python_Crash_Course/Files_and_exceptions/file_reader.py
+++ Python_Crash_Course/Files_and_exceptions/file_reader.py
@@ -1,7 +1,14 @@
-#open() function retuns an object representing the file 
-# python assigns this object to file_object
-with open('pi_digits.txt') as file_object:
-    contents = file_object.read()
-print(contents)
+print("Give me two numbers I will divide them.")
+print("enter q to quit")
 
+while True:
+    first_num = input("Enter first number : ")
+    if first_num == "q":
+        break
 
+    sec_num = input("enter the second number")
+    if sec_num == "q":
+        break
+
+    ans = int(first_num) / int(sec_num)
+    print(ans)
