251d81fb16ccbe7082701d49ec80c0b51336341e
abhiram-ar <82314439+abhiram-ar@users.noreply.github.com>
2024-06-26 22:23:30 +0530

    finish class test

--- Python_Crash_Course/Testing_your_code/Testing a class/test_survey.py
+++ Python_Crash_Course/Testing_your_code/Testing a class/test_survey.py
@@ -11,6 +11,18 @@ class TestAnonumousSurvey(unittest.TestCase):
         my_survey = AnonymousSurvey(question)
         my_survey.store_response('English')
         self.assertIn('English',my_survey.responses)
+    
+    def test_store_three_responses(self):
+        """test that three individial responses are stored properly"""
+
+        question = "what language did you firt learn to speak?"
+        my_survey = AnonymousSurvey(question)
+        responses = ['english', 'malayalam', 'hindi']
+        for response in responses:
+            my_survey.store_response(response)
+
+        for response in responses:
+            self.assertIn(response, my_survey.responses)
 
 
 if __name__ == "__main__":
---
+++ Python_Crash_Course/Testing_your_code/Testing a class/test_survey_2.py
@@ -0,0 +1,40 @@
+import unittest
+from survey import AnonymousSurvey
+
+class TestAnonumousSurvey(unittest.TestCase):
+    """Test for the class AnonymousSurvey"""
+    
+
+    # The unittest.TestCase class has a setUp() method that 
+    # allows you to create these objects once and then 
+    # use them in each of your test methods
+    def setUp(self):
+        """create a survey and a set of responsed for use in all test methods"""
+        question = "what language did you first learn to speak?"
+        self.my_survey = AnonymousSurvey(question)
+        self.responses = ['english', 'malayalam', 'hindi']
+
+        
+
+    def test_store_single_response(self):
+        """Test that a single response is stored properly."""
+
+        self.my_survey.store_response(self.responses[0])
+        self.assertIn(self.responses[0],self.my_survey.responses)
+    
+    def test_store_three_responses(self):
+        """test that three individial responses are stored properly"""
+
+        for response in self.responses:
+            self.my_survey.store_response(response)
+
+        for response in self.responses:
+            self.assertIn(response, self.my_survey.responses)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+
+# there is some repetiion going on this test code
+# to avoid this refer test_survey_2.py
