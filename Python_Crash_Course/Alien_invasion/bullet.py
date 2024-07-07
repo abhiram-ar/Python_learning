52d1c97d77ee99e4dbc60eef39575eef5f63b777
abhiram-ar <82314439+abhiram-ar@users.noreply.github.com>
2024-07-08 05:15:16 +0530

    bullets

---
+++ Python_Crash_Course/Alien_invasion/bullet.py
@@ -0,0 +1,27 @@
+import pygame
+from pygame.sprite import Sprite
+
+# When you use sprites, you can group related elements in 
+# your game and act on all the grouped elements at once
+
+class Bullet(Sprite):
+    """a class to manage bullets fired from the ship"""
+
+    def __init__(self, ai_game):
+        """create a bullet object at ships current position"""
+        super().__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.color = self.settings.bullet_color
+
+
+        # create a bullet rect at (0,0) and then set correct position
+        self.rect = pygame.Rect(0,0, self.settings.bullet_width, self.settings.bullet_height)
+        self.rect.midbottom = ai_game.ship.rect.midtop
+
+
+        # store the bullet's position as a decimal value.
+        self.y = float(self.rect.y)
+
+
+
--- Python_Crash_Course/Alien_invasion/settings.py
+++ Python_Crash_Course/Alien_invasion/settings.py
@@ -4,10 +4,16 @@ class Settings:
     def __init__(self):
         """initialize the game's settings"""
 
-        #screen settings
+        # screen settings
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
         
-        #ship settings
+        # ship settings
         self.ship_speed = 1.5
+
+        # bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
