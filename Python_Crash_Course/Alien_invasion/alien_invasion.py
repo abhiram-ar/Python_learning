e18b2bb9206882fbb8c95e9e827c88cb5c4e71d5
abhiram-ar <82314439+abhiram-ar@users.noreply.github.com>
2024-07-24 01:39:40 +0530

    mouse visibility during gameplay

--- Python_Crash_Course/Alien_invasion/alien_invasion.py
+++ Python_Crash_Course/Alien_invasion/alien_invasion.py
@@ -152,6 +152,9 @@ class AlienInvasion:
             self._create_fleet()
             self.ship.center_ship()
 
+            # hide the mouse cursor
+            pygame.mouse.set_visible(False)
+
 
 
 
@@ -243,7 +246,7 @@ class AlienInvasion:
         
         else:
             self.stats.game_active = False
-
+            pygame.mouse.set_visible(True) # show the mouse again so the player can choose
 
     def _check_aliens_bottom(self):
         """"check if any aliens have reached the bottom of the screen"""
